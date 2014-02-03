FMCorz/moodle

MDL-43858 atto_charmap: New plugin to insert special characters

lib/editor/atto/plugins/charmap/lang/en/atto_charmap.php
@@ -1,0 +1,282 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'atto_charmap', language 'en'.
+ *
+ * @package    atto_charmap
+ * @copyright  2014 Frédéric Massart
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['insertcharacter'] = 'Insert character';
+$string['pluginname'] = 'Insert character';
+
+// The following are not ordered on purpose, they are kept
+// in the order in which they appear in the user interface.
+$string['nobreakspace'] = 'no-break space';
+$string['ampersand'] = 'ampersand';
+$string['quotationmark'] = 'quotation mark';
+$string['centsign'] = 'cent sign';
+$string['eurosign'] = 'euro sign';
+$string['poundsign'] = 'pound sign';
+$string['yensign'] = 'yen sign';
+$string['copyrightsign'] = 'copyright sign';
+$string['registeredsign'] = 'registered sign';
+$string['trademarksign'] = 'trade mark sign';
+$string['permillesign'] = 'per mille sign';
+$string['microsign'] = 'micro sign';
+$string['middledot'] = 'middle dot';
+$string['bullet'] = 'bullet';
+$string['threedotleader'] = 'three dot leader';
+$string['minutesfeet'] = 'minutes / feet';
+$string['secondsinches'] = 'seconds / inches';
+$string['sectionsign'] = 'section sign';
+$string['paragraphsign'] = 'paragraph sign';
+$string['sharpsesszed'] = 'sharp s / ess-zed';
+$string['singleleftpointinganglequotationmark'] = 'single left-pointing angle quotation mark';
+$string['singlerightpointinganglequotationmark'] = 'single right-pointing angle quotation mark';
+$string['leftpointingguillemet'] = 'left pointing guillemet';
+$string['rightpointingguillemet'] = 'right pointing guillemet';
+$string['leftsinglequotationmark'] = 'left single quotation mark';
+$string['rightsinglequotationmark'] = 'right single quotation mark';
+$string['leftdoublequotationmark'] = 'left double quotation mark';
+$string['rightdoublequotationmark'] = 'right double quotation mark';
+$string['singlelow9quotationmark'] = 'single low-9 quotation mark';
+$string['doublelow9quotationmark'] = 'double low-9 quotation mark';
+$string['lessthansign'] = 'less-than sign';
+$string['greaterthansign'] = 'greater-than sign';
+$string['lessthanorequalto'] = 'less-than or equal to';
+$string['greaterthanorequalto'] = 'greater-than or equal to';
+$string['endash'] = 'en dash';
+$string['emdash'] = 'em dash';
+$string['macron'] = 'macron';
+$string['overline'] = 'overline';
+$string['currencysign'] = 'currency sign';
+$string['brokenbar'] = 'broken bar';
+$string['diaeresis'] = 'diaeresis';
+$string['invertedexclamationmark'] = 'inverted exclamation mark';
+$string['turnedquestionmark'] = 'turned question mark';
+$string['circumflexaccent'] = 'circumflex accent';
+$string['smalltilde'] = 'small tilde';
+$string['degreesign'] = 'degree sign';
+$string['minussign'] = 'minus sign';
+$string['plusminussign'] = 'plus-minus sign';
+$string['divisionsign'] = 'division sign';
+$string['fractionslash'] = 'fraction slash';
+$string['multiplicationsign'] = 'multiplication sign';
+$string['superscriptone'] = 'superscript one';
+$string['superscripttwo'] = 'superscript two';
+$string['superscriptthree'] = 'superscript three';
+$string['fractiononequarter'] = 'fraction one quarter';
+$string['fractiononehalf'] = 'fraction one half';
+$string['fractionthreequarters'] = 'fraction three quarters';
+$string['functionflorin'] = 'function / florin';
+$string['integral'] = 'integral';
+$string['narysumation'] = 'n-ary sumation';
+$string['infinity'] = 'infinity';
+$string['squareroot'] = 'square root';
+$string['similarto'] = 'similar to';
+$string['approximatelyequalto'] = 'approximately equal to';
+$string['almostequalto'] = 'almost equal to';
+$string['notequalto'] = 'not equal to';
+$string['identicalto'] = 'identical to';
+$string['elementof'] = 'element of';
+$string['notanelementof'] = 'not an element of';
+$string['containsasmember'] = 'contains as member';
+$string['naryproduct'] = 'n-ary product';
+$string['logicaland'] = 'logical and';
+$string['logicalor'] = 'logical or';
+$string['notsign'] = 'not sign';
+$string['intersection'] = 'intersection';
+$string['union'] = 'union';
+$string['partialdifferential'] = 'partial differential';
+$string['forall'] = 'for all';
+$string['thereexists'] = 'there exists';
+$string['diameter'] = 'diameter';
+$string['backwarddifference'] = 'backward difference';
+$string['asteriskoperator'] = 'asterisk operator';
+$string['proportionalto'] = 'proportional to';
+$string['angle'] = 'angle';
+$string['acuteaccent'] = 'acute accent';
+$string['cedilla'] = 'cedilla';
+$string['feminineordinalindicator'] = 'feminine ordinal indicator';
+$string['masculineordinalindicator'] = 'masculine ordinal indicator';
+$string['dagger'] = 'dagger';
+$string['doubledagger'] = 'double dagger';
+$string['agrave'] = 'A - grave';
+$string['aacute'] = 'A - acute';
+$string['acircumflex'] = 'A - circumflex';
+$string['atilde'] = 'A - tilde';
+$string['adiaeresis'] = 'A - diaeresis';
+$string['aringabove'] = 'A - ring above';
+$string['ligatureae'] = 'ligature AE';
+$string['ccedilla'] = 'C - cedilla';
+$string['egrave'] = 'E - grave';
+$string['eacute'] = 'E - acute';
+$string['ecircumflex'] = 'E - circumflex';
+$string['ediaeresis'] = 'E - diaeresis';
+$string['igrave'] = 'I - grave';
+$string['iacute'] = 'I - acute';
+$string['icircumflex'] = 'I - circumflex';
+$string['idiaeresis'] = 'I - diaeresis';
+$string['eth'] = 'ETH';
+$string['ntilde'] = 'N - tilde';
+$string['ograve'] = 'O - grave';
+$string['oacute'] = 'O - acute';
+$string['ocircumflex'] = 'O - circumflex';
+$string['otilde'] = 'O - tilde';
+$string['odiaeresis'] = 'O - diaeresis';
+$string['oslash'] = 'O - slash';
+$string['ligatureoe'] = 'ligature OE';
+$string['scaron'] = 'S - caron';
+$string['ugrave'] = 'U - grave';
+$string['uacute'] = 'U - acute';
+$string['ucircumflex'] = 'U - circumflex';
+$string['udiaeresis'] = 'U - diaeresis';
+$string['yacute'] = 'Y - acute';
+$string['ydiaeresis'] = 'Y - diaeresis';
+$string['thorn'] = 'THORN';
+$string['agrave'] = 'a - grave';
+$string['aacute'] = 'a - acute';
+$string['acircumflex'] = 'a - circumflex';
+$string['atilde'] = 'a - tilde';
+$string['adiaeresis'] = 'a - diaeresis';
+$string['aringabove'] = 'a - ring above';
+$string['ligatureae'] = 'ligature ae';
+$string['ccedilla'] = 'c - cedilla';
+$string['egrave'] = 'e - grave';
+$string['eacute'] = 'e - acute';
+$string['ecircumflex'] = 'e - circumflex';
+$string['ediaeresis'] = 'e - diaeresis';
+$string['igrave'] = 'i - grave';
+$string['iacute'] = 'i - acute';
+$string['icircumflex'] = 'i - circumflex';
+$string['idiaeresis'] = 'i - diaeresis';
+$string['eth'] = 'eth';
+$string['ntilde'] = 'n - tilde';
+$string['ograve'] = 'o - grave';
+$string['oacute'] = 'o - acute';
+$string['ocircumflex'] = 'o - circumflex';
+$string['otilde'] = 'o - tilde';
+$string['odiaeresis'] = 'o - diaeresis';
+$string['oslash'] = 'o slash';
+$string['ligatureoe'] = 'ligature oe';
+$string['scaron'] = 's - caron';
+$string['ugrave'] = 'u - grave';
+$string['uacute'] = 'u - acute';
+$string['ucircumflex'] = 'u - circumflex';
+$string['udiaeresis'] = 'u - diaeresis';
+$string['yacute'] = 'y - acute';
+$string['thorn'] = 'thorn';
+$string['ydiaeresis'] = 'y - diaeresis';
+$string['alpha'] = 'Alpha';
+$string['beta'] = 'Beta';
+$string['gamma'] = 'Gamma';
+$string['delta'] = 'Delta';
+$string['epsilon'] = 'Epsilon';
+$string['zeta'] = 'Zeta';
+$string['eta'] = 'Eta';
+$string['theta'] = 'Theta';
+$string['iota'] = 'Iota';
+$string['kappa'] = 'Kappa';
+$string['lambda'] = 'Lambda';
+$string['mu'] = 'Mu';
+$string['nu'] = 'Nu';
+$string['xi'] = 'Xi';
+$string['omicron'] = 'Omicron';
+$string['pi'] = 'Pi';
+$string['rho'] = 'Rho';
+$string['sigma'] = 'Sigma';
+$string['tau'] = 'Tau';
+$string['upsilon'] = 'Upsilon';
+$string['phi'] = 'Phi';
+$string['chi'] = 'Chi';
+$string['psi'] = 'Psi';
+$string['omega'] = 'Omega';
+$string['alpha'] = 'alpha';
+$string['beta'] = 'beta';
+$string['gamma'] = 'gamma';
+$string['delta'] = 'delta';
+$string['epsilon'] = 'epsilon';
+$string['zeta'] = 'zeta';
+$string['eta'] = 'eta';
+$string['theta'] = 'theta';
+$string['iota'] = 'iota';
+$string['kappa'] = 'kappa';
+$string['lambda'] = 'lambda';
+$string['mu'] = 'mu';
+$string['nu'] = 'nu';
+$string['xi'] = 'xi';
+$string['omicron'] = 'omicron';
+$string['pi'] = 'pi';
+$string['rho'] = 'rho';
+$string['finalsigma'] = 'final sigma';
+$string['sigma'] = 'sigma';
+$string['tau'] = 'tau';
+$string['upsilon'] = 'upsilon';
+$string['phi'] = 'phi';
+$string['chi'] = 'chi';
+$string['psi'] = 'psi';
+$string['omega'] = 'omega';
+$string['alefsymbol'] = 'alef symbol';
+$string['pisymbol'] = 'pi symbol';
+$string['realpartsymbol'] = 'real part symbol';
+$string['thetasymbol'] = 'theta symbol';
+$string['upsilonhooksymbol'] = 'upsilon - hook symbol';
+$string['weierstrassp'] = 'Weierstrass p';
+$string['imaginarypart'] = 'imaginary part';
+$string['leftwardsarrow'] = 'leftwards arrow';
+$string['upwardsarrow'] = 'upwards arrow';
+$string['rightwardsarrow'] = 'rightwards arrow';
+$string['downwardsarrow'] = 'downwards arrow';
+$string['leftrightarrow'] = 'left right arrow';
+$string['carriagereturn'] = 'carriage return';
+$string['leftwardsdoublearrow'] = 'leftwards double arrow';
+$string['upwardsdoublearrow'] = 'upwards double arrow';
+$string['rightwardsdoublearrow'] = 'rightwards double arrow';
+$string['downwardsdoublearrow'] = 'downwards double arrow';
+$string['leftrightdoublearrow'] = 'left right double arrow';
+$string['therefore'] = 'therefore';
+$string['subsetof'] = 'subset of';
+$string['supersetof'] = 'superset of';
+$string['notasubsetof'] = 'not a subset of';
+$string['subsetoforequalto'] = 'subset of or equal to';
+$string['supersetoforequalto'] = 'superset of or equal to';
+$string['circledplus'] = 'circled plus';
+$string['circledtimes'] = 'circled times';
+$string['perpendicular'] = 'perpendicular';
+$string['dotoperator'] = 'dot operator';
+$string['leftceiling'] = 'left ceiling';
+$string['rightceiling'] = 'right ceiling';
+$string['leftfloor'] = 'left floor';
+$string['rightfloor'] = 'right floor';
+$string['leftpointinganglebracket'] = 'left-pointing angle bracket';
+$string['rightpointinganglebracket'] = 'right-pointing angle bracket';
+$string['lozenge'] = 'lozenge';
+$string['blackspadesuit'] = 'black spade suit';
+$string['blackclubsuit'] = 'black club suit';
+$string['blackheartsuit'] = 'black heart suit';
+$string['blackdiamondsuit'] = 'black diamond suit';
+$string['enspace'] = 'en space';
+$string['emspace'] = 'em space';
+$string['thinspace'] = 'thin space';
+$string['zerowidthnonjoiner'] = 'zero width non-joiner';
+$string['zerowidthjoiner'] = 'zero width joiner';
+$string['lefttorightmark'] = 'left-to-right mark';
+$string['righttoleftmark'] = 'right-to-left mark';
+$string['softhyphen'] = 'soft hyphen';
+$string['eacute'] = 'eacute';

rwijaya/atto_27

lib/editor/atto/plugins/charmap/lang/en/atto_charmap.php
@@ -1,0 +1,281 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'atto_charmap', language 'en'.
+ *
+ * @package    atto_charmap
+ * @copyright  2014 Frédéric Massart
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['insertcharacter'] = 'Insert character';
+$string['pluginname'] = 'Insert character';
+
+// The following are not ordered on purpose, they are kept
+// in the order in which they appear in the user interface.
+$string['nobreakspace'] = 'no-break space';
+$string['ampersand'] = 'ampersand';
+$string['quotationmark'] = 'quotation mark';
+$string['centsign'] = 'cent sign';
+$string['eurosign'] = 'euro sign';
+$string['poundsign'] = 'pound sign';
+$string['yensign'] = 'yen sign';
+$string['copyrightsign'] = 'copyright sign';
+$string['registeredsign'] = 'registered sign';
+$string['trademarksign'] = 'trade mark sign';
+$string['permillesign'] = 'per mille sign';
+$string['microsign'] = 'micro sign';
+$string['middledot'] = 'middle dot';
+$string['bullet'] = 'bullet';
+$string['threedotleader'] = 'three dot leader';
+$string['minutesfeet'] = 'minutes / feet';
+$string['secondsinches'] = 'seconds / inches';
+$string['sectionsign'] = 'section sign';
+$string['paragraphsign'] = 'paragraph sign';
+$string['sharpsesszed'] = 'sharp s / ess-zed';
+$string['singleleftpointinganglequotationmark'] = 'single left-pointing angle quotation mark';
+$string['singlerightpointinganglequotationmark'] = 'single right-pointing angle quotation mark';
+$string['leftpointingguillemet'] = 'left pointing guillemet';
+$string['rightpointingguillemet'] = 'right pointing guillemet';
+$string['leftsinglequotationmark'] = 'left single quotation mark';
+$string['rightsinglequotationmark'] = 'right single quotation mark';
+$string['leftdoublequotationmark'] = 'left double quotation mark';
+$string['rightdoublequotationmark'] = 'right double quotation mark';
+$string['singlelow9quotationmark'] = 'single low-9 quotation mark';
+$string['doublelow9quotationmark'] = 'double low-9 quotation mark';
+$string['lessthansign'] = 'less-than sign';
+$string['greaterthansign'] = 'greater-than sign';
+$string['lessthanorequalto'] = 'less-than or equal to';
+$string['greaterthanorequalto'] = 'greater-than or equal to';
+$string['endash'] = 'en dash';
+$string['emdash'] = 'em dash';
+$string['macron'] = 'macron';
+$string['overline'] = 'overline';
+$string['currencysign'] = 'currency sign';
+$string['brokenbar'] = 'broken bar';
+$string['diaeresis'] = 'diaeresis';
+$string['invertedexclamationmark'] = 'inverted exclamation mark';
+$string['turnedquestionmark'] = 'turned question mark';
+$string['circumflexaccent'] = 'circumflex accent';
+$string['smalltilde'] = 'small tilde';
+$string['degreesign'] = 'degree sign';
+$string['minussign'] = 'minus sign';
+$string['plusminussign'] = 'plus-minus sign';
+$string['divisionsign'] = 'division sign';
+$string['fractionslash'] = 'fraction slash';
+$string['multiplicationsign'] = 'multiplication sign';
+$string['superscriptone'] = 'superscript one';
+$string['superscripttwo'] = 'superscript two';
+$string['superscriptthree'] = 'superscript three';
+$string['fractiononequarter'] = 'fraction one quarter';
+$string['fractiononehalf'] = 'fraction one half';
+$string['fractionthreequarters'] = 'fraction three quarters';
+$string['functionflorin'] = 'function / florin';
+$string['integral'] = 'integral';
+$string['narysumation'] = 'n-ary sumation';
+$string['infinity'] = 'infinity';
+$string['squareroot'] = 'square root';
+$string['similarto'] = 'similar to';
+$string['approximatelyequalto'] = 'approximately equal to';
+$string['almostequalto'] = 'almost equal to';
+$string['notequalto'] = 'not equal to';
+$string['identicalto'] = 'identical to';
+$string['elementof'] = 'element of';
+$string['notanelementof'] = 'not an element of';
+$string['containsasmember'] = 'contains as member';
+$string['naryproduct'] = 'n-ary product';
+$string['logicaland'] = 'logical and';
+$string['logicalor'] = 'logical or';
+$string['notsign'] = 'not sign';
+$string['intersection'] = 'intersection';
+$string['union'] = 'union';
+$string['partialdifferential'] = 'partial differential';
+$string['forall'] = 'for all';
+$string['thereexists'] = 'there exists';
+$string['diameter'] = 'diameter';
+$string['backwarddifference'] = 'backward difference';
+$string['asteriskoperator'] = 'asterisk operator';
+$string['proportionalto'] = 'proportional to';
+$string['angle'] = 'angle';
+$string['acuteaccent'] = 'acute accent';
+$string['cedilla'] = 'cedilla';
+$string['feminineordinalindicator'] = 'feminine ordinal indicator';
+$string['masculineordinalindicator'] = 'masculine ordinal indicator';
+$string['dagger'] = 'dagger';
+$string['doubledagger'] = 'double dagger';
+$string['agrave_caps'] = 'A - grave';
+$string['aacute_caps'] = 'A - acute';
+$string['acircumflex_caps'] = 'A - circumflex';
+$string['atilde_caps'] = 'A - tilde';
+$string['adiaeresis_caps'] = 'A - diaeresis';
+$string['aringabove_caps'] = 'A - ring above';
+$string['ligatureae_caps'] = 'ligature AE';
+$string['ccedilla_caps'] = 'C - cedilla';
+$string['egrave_caps'] = 'E - grave';
+$string['eacute_caps'] = 'E - acute';
+$string['ecircumflex_caps'] = 'E - circumflex';
+$string['ediaeresis_caps'] = 'E - diaeresis';
+$string['igrave_caps'] = 'I - grave';
+$string['iacute_caps'] = 'I - acute';
+$string['icircumflex_caps'] = 'I - circumflex';
+$string['idiaeresis_caps'] = 'I - diaeresis';
+$string['eth_caps'] = 'ETH';
+$string['ntilde_caps'] = 'N - tilde';
+$string['ograve_caps'] = 'O - grave';
+$string['oacute_caps'] = 'O - acute';
+$string['ocircumflex_caps'] = 'O - circumflex';
+$string['otilde_caps'] = 'O - tilde';
+$string['odiaeresis_caps'] = 'O - diaeresis';
+$string['oslash_caps'] = 'O - slash';
+$string['ligatureoe_caps'] = 'ligature OE';
+$string['scaron_caps'] = 'S - caron';
+$string['ugrave_caps'] = 'U - grave';
+$string['uacute_caps'] = 'U - acute';
+$string['ucircumflex_caps'] = 'U - circumflex';
+$string['udiaeresis_caps'] = 'U - diaeresis';
+$string['yacute_caps'] = 'Y - acute';
+$string['ydiaeresis_caps'] = 'Y - diaeresis';
+$string['thorn_caps'] = 'THORN';
+$string['agrave'] = 'a - grave';
+$string['aacute'] = 'a - acute';
+$string['acircumflex'] = 'a - circumflex';
+$string['atilde'] = 'a - tilde';
+$string['adiaeresis'] = 'a - diaeresis';
+$string['aringabove'] = 'a - ring above';
+$string['ligatureae'] = 'ligature ae';
+$string['ccedilla'] = 'c - cedilla';
+$string['egrave'] = 'e - grave';
+$string['eacute'] = 'e - acute';
+$string['ecircumflex'] = 'e - circumflex';
+$string['ediaeresis'] = 'e - diaeresis';
+$string['igrave'] = 'i - grave';
+$string['iacute'] = 'i - acute';
+$string['icircumflex'] = 'i - circumflex';
+$string['idiaeresis'] = 'i - diaeresis';
+$string['eth'] = 'eth';
+$string['ntilde'] = 'n - tilde';
+$string['ograve'] = 'o - grave';
+$string['oacute'] = 'o - acute';
+$string['ocircumflex'] = 'o - circumflex';
+$string['otilde'] = 'o - tilde';
+$string['odiaeresis'] = 'o - diaeresis';
+$string['oslash'] = 'o slash';
+$string['ligatureoe'] = 'ligature oe';
+$string['scaron'] = 's - caron';
+$string['ugrave'] = 'u - grave';
+$string['uacute'] = 'u - acute';
+$string['ucircumflex'] = 'u - circumflex';
+$string['udiaeresis'] = 'u - diaeresis';
+$string['yacute'] = 'y - acute';
+$string['thorn'] = 'thorn';
+$string['ydiaeresis'] = 'y - diaeresis';
+$string['alpha_caps'] = 'Alpha';
+$string['beta_caps'] = 'Beta';
+$string['gamma_caps'] = 'Gamma';
+$string['delta_caps'] = 'Delta';
+$string['epsilon_caps'] = 'Epsilon';
+$string['zeta_caps'] = 'Zeta';
+$string['eta_caps'] = 'Eta';
+$string['theta_caps'] = 'Theta';
+$string['iota_caps'] = 'Iota';
+$string['kappa_caps'] = 'Kappa';
+$string['lambda_caps'] = 'Lambda';
+$string['mu_caps'] = 'Mu';
+$string['nu_caps'] = 'Nu';
+$string['xi_caps'] = 'Xi';
+$string['omicron_caps'] = 'Omicron';
+$string['pi_caps'] = 'Pi';
+$string['rho_caps'] = 'Rho';
+$string['sigma_caps'] = 'Sigma';
+$string['tau_caps'] = 'Tau';
+$string['upsilon_caps'] = 'Upsilon';
+$string['phi_caps'] = 'Phi';
+$string['chi_caps'] = 'Chi';
+$string['psi_caps'] = 'Psi';
+$string['omega_caps'] = 'Omega';
+$string['alpha'] = 'alpha';
+$string['beta'] = 'beta';
+$string['gamma'] = 'gamma';
+$string['delta'] = 'delta';
+$string['epsilon'] = 'epsilon';
+$string['zeta'] = 'zeta';
+$string['eta'] = 'eta';
+$string['theta'] = 'theta';
+$string['iota'] = 'iota';
+$string['kappa'] = 'kappa';
+$string['lambda'] = 'lambda';
+$string['mu'] = 'mu';
+$string['nu'] = 'nu';
+$string['xi'] = 'xi';
+$string['omicron'] = 'omicron';
+$string['pi'] = 'pi';
+$string['rho'] = 'rho';
+$string['finalsigma'] = 'final sigma';
+$string['sigma'] = 'sigma';
+$string['tau'] = 'tau';
+$string['upsilon'] = 'upsilon';
+$string['phi'] = 'phi';
+$string['chi'] = 'chi';
+$string['psi'] = 'psi';
+$string['omega'] = 'omega';
+$string['alefsymbol'] = 'alef symbol';
+$string['pisymbol'] = 'pi symbol';
+$string['realpartsymbol'] = 'real part symbol';
+$string['thetasymbol'] = 'theta symbol';
+$string['upsilonhooksymbol'] = 'upsilon - hook symbol';
+$string['weierstrassp'] = 'Weierstrass p';
+$string['imaginarypart'] = 'imaginary part';
+$string['leftwardsarrow'] = 'leftwards arrow';
+$string['upwardsarrow'] = 'upwards arrow';
+$string['rightwardsarrow'] = 'rightwards arrow';
+$string['downwardsarrow'] = 'downwards arrow';
+$string['leftrightarrow'] = 'left right arrow';
+$string['carriagereturn'] = 'carriage return';
+$string['leftwardsdoublearrow'] = 'leftwards double arrow';
+$string['upwardsdoublearrow'] = 'upwards double arrow';
+$string['rightwardsdoublearrow'] = 'rightwards double arrow';
+$string['downwardsdoublearrow'] = 'downwards double arrow';
+$string['leftrightdoublearrow'] = 'left right double arrow';
+$string['therefore'] = 'therefore';
+$string['subsetof'] = 'subset of';
+$string['supersetof'] = 'superset of';
+$string['notasubsetof'] = 'not a subset of';
+$string['subsetoforequalto'] = 'subset of or equal to';
+$string['supersetoforequalto'] = 'superset of or equal to';
+$string['circledplus'] = 'circled plus';
+$string['circledtimes'] = 'circled times';
+$string['perpendicular'] = 'perpendicular';
+$string['dotoperator'] = 'dot operator';
+$string['leftceiling'] = 'left ceiling';
+$string['rightceiling'] = 'right ceiling';
+$string['leftfloor'] = 'left floor';
+$string['rightfloor'] = 'right floor';
+$string['leftpointinganglebracket'] = 'left-pointing angle bracket';
+$string['rightpointinganglebracket'] = 'right-pointing angle bracket';
+$string['lozenge'] = 'lozenge';
+$string['blackspadesuit'] = 'black spade suit';
+$string['blackclubsuit'] = 'black club suit';
+$string['blackheartsuit'] = 'black heart suit';
+$string['blackdiamondsuit'] = 'black diamond suit';
+$string['enspace'] = 'en space';
+$string['emspace'] = 'em space';
+$string['thinspace'] = 'thin space';
+$string['zerowidthnonjoiner'] = 'zero width non-joiner';
+$string['zerowidthjoiner'] = 'zero width joiner';
+$string['lefttorightmark'] = 'left-to-right mark';
+$string['righttoleftmark'] = 'right-to-left mark';
+$string['softhyphen'] = 'soft hyphen';